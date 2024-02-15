e08a3147027498cd19c573985a2fc7aa03037790
Wizzard2001 <rpossberg1@nait.ca>
2024-02-15 14:08:31 -0700

    Refactor link styling in main.js

--- js/main.js
+++ js/main.js
@@ -15,32 +15,27 @@ function onHandleViewResources() {
 }
 
 /*
-    Task 2 Making the links bold.
+    Task 2 Making the text links bold.
     1. add an event listener on to the div element with the id of resources that listens to the click event.
     2. Make sure to pass the event object to the event handler function.
     3. Using the console.log() method, display the target property of the event object.
     4. On the event.target use the method classList.add to add the class named fw-bold.
 */
+
 // Get all link elements within the resources div
 let links = resources.getElementsByTagName("a");
 
 // Add mouseover and mouseout event listeners to each link
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("mouseover", function () {
-    this.classList.add("fw-bold");
+    // Add the fw-bold class to the parent div of the link
+    this.parentElement.classList.add("fw-bold");
   });
   links[i].addEventListener("mouseout", function () {
-    this.classList.remove("fw-bold");
+    // Remove the fw-bold class from the parent div of the link
+    this.parentElement.classList.remove("fw-bold");
   });
 }
-
-/*  Task 3 - Remove the bold font form the links
-     1. Add an event listener on to the div element with the id of resources that listens to the mouseout event.
-     2. Listen to the mouseout event on the div element with the id of resources.
-     3. Create an event handler function that removes the class fw-bold from the event.target.
-     4. Pass the event object to the event handler function.
-     5. Inside the event handler remove the bold text from the event.target using the classList.remove method.
-     */
 
 // Add an event listener to the resources div that listens for the mouseout event.
 resources.addEventListener("mouseout", onHandleMouseOut);
